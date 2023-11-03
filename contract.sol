05af0dd99e61bd400f4ae1e4412534a27a3ad219
Metacrafters0x1 <149813536+Metacrafters0x1@users.noreply.github.com>
2023-11-03 21:32:46 +0530

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
+contract firstcontract {
+
+    string public name;
+    string public symbol;
+    uint256 public totalSupply=0;
+
+    mapping(address => uint256) balances;
+
+    constructor(string memory tokenName, string memory tokenSymbol) {
+        name = tokenName;
+        symbol = tokenSymbol;
+    }
+
+    function getbalance(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+    function mint(address account, uint256 amount) public {
+        totalSupply += amount;
+        balances[account] += amount;
+    }
+
+    function burn(address account, uint256 amount) public {    
+        require(balances[account] >= amount, "Sorry low balance than requested");
+        totalSupply -= amount;
+        balances[account] -= amount;
+    }
+}
